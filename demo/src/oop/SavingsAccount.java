339503b299b1da27d254a39ddaecb06c2c18cab7
srikanthpragada <srikanthpragada@gmail.com>
2021-02-02 18:53:54 +0530

    02-FEB

--- demo/src/oop/SavingsAccount.java
+++ demo/src/oop/SavingsAccount.java
@@ -6,16 +6,32 @@ public class SavingsAccount {
    private String ahname;
    private double balance;
    
-   // Methods 
-   public void open(int no, String name) {
+   // Constructor 
+   public SavingsAccount(int no, String name) {
 	   acno = no;
 	   ahname = name;
 	   balance = 0;
    }
    
+   // Methods 
    public void print() {
 	   System.out.println(acno);
 	   System.out.println(ahname);
 	   System.out.println(balance);
+   }
+   
+   public void deposit(double amount) {
+	   balance += amount;
+   }
+   
+   public void withdraw(double amount) {
+	   if (balance >= amount)
+	       balance -= amount;
+	   else
+		   System.out.println("Insufficient Balance");
+   }
+
+   public double getBalance() {
+	   return balance;
    }
 }
--- demo/src/oop/TestSavingsAccount.java
+++ demo/src/oop/TestSavingsAccount.java
@@ -5,10 +5,13 @@ public class TestSavingsAccount {
 	public static void main(String[] args) {
 		SavingsAccount sa; // object reference 
 		
-		  sa = new SavingsAccount(); // create object 
-		  // sa.acno = 10;
-		  sa.open(1, "Steve"); // call method
-		  sa.print();
+		  sa = new SavingsAccount(1,"Steve"); // create object or instance
+		  sa.deposit(5000);
+		  sa.deposit(10000);
+		  sa.withdraw(12000);
+		  System.out.println(sa.getBalance());
+		  
+		 
 	}
 
 }
