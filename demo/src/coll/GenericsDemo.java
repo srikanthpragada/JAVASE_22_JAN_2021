e4fe04af10edad49610954b78a0dff974da8f952
srikanthpragada <srikanthpragada@gmail.com>
2021-03-01 18:55:58 +0530

    01-MAR

--- demo/src/coll/GenericsDemo.java
+++ demo/src/coll/GenericsDemo.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class GenericsDemo {
 
 	public static void main(String[] args) {
-		 ArrayList<String> al = new ArrayList<String>();
+		 var al = new ArrayList<String>();
 		 
-		 al.add("Abc");
 		 // al.add(10);
+		 al.add("Abc");
 	 
 		 
 		 String s =  al.get(0);
